f7fb8bce1aa4a32e01afb671b282260a84968c1d
Sacha_Sanchez <amiralpingouin@gmail.com>
2025-08-03 16:50:27 +0200

    Ajout de la route pour les lieux - V1.11.3

--- routes/placesRoute.js
+++ routes/placesRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Place } = require('../models/placesModel');
+const { Place, getPlaceById } = require('../models/placesModel');
 
 router.get('/', async (req, res) => {
     try {
@@ -8,6 +8,17 @@ router.get('/', async (req, res) => {
         res.json(places);
     } catch (err) {
         res.status(500).json({ error: 'Erreur serveur' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const place = await getPlaceById(req.params.id);
+        if (!place) return res.status(404).json({ success: false, message: "Lieu introuvable" });
+        res.json({ success: true, place });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, message: "Erreur serveur" });
     }
 });
 
